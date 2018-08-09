f530526301edf8f8538a5e95e9da1e67fd9ceb6a
Snehi <jsnehi@lm-maa-22017422.paypalcorp.com>
2018-08-09 13:42:51 +0530

    initial commit

--- src/SpecialList.java
+++ src/SpecialList.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class SpecialList {
+
+    public ArrayList<Integer> seq;
+//
+//    public SpecialList(int x ){
+//        seq = new ArrayList<>();
+//    }
+
+    public int getNextVal(int currVal){
+        return getNextNth(currVal,1);
+    }
+
+    public int getPrevVal(int currVal){
+        return getPrevNth(currVal,-1);
+    }
+
+    public int getDiff(int val1,int val2){
+        int min = Math.min(val1,val2);
+        int max = Math.max(val1,val2);
+        int count=0;
+        while(min!=max){
+            min = getNextVal(min);
+            count++;
+        }
+        return count;
+    }
+
+    public int getNextNth(int currVal,int n){
+        int index = seq.indexOf(currVal);
+        return seq.get((index +n)%seq.size());
+    }
+
+    public int getPrevNth(int currVal,int n){
+        int index = seq.indexOf(currVal);
+        return seq.get((index-n)%seq.size());
+    }
+
 }
