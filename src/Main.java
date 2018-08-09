55ebc5c2b8cb72b39cc8cf199d34fae3ede3dc04
srdesai <isriramdesai@gmail.com>
2018-08-09 13:42:40 +0530

    Added the constructor of SpecialList.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String [] args) {
-
+        SpecialList specialList = new SpecialList(4);
     }
 
 }
--- src/SpecialList.java
+++ src/SpecialList.java
@@ -1,2 +1,67 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpecialList {
+
+    private List<Integer> seq;
+
+    private List<Integer> toDigits(int n) {
+        List<Integer> digits = new ArrayList<>();
+        while(n > 0) {
+            digits.add(n % 10);
+            n /= 10;
+        }
+        return digits;
+    }
+
+    private int toNumber(List<Integer> digits) {
+        int number = 0;
+        for(int i=0;i<digits.size();i++) {
+            number = (number * 10) + digits.get(i);
+        }
+        return number;
+    }
+
+    private int getSmallestNumber(int size) {
+        int smallestNumber = 0;
+        for(int i=1;i<=size;i++) {
+            smallestNumber = (smallestNumber * 10) + i;
+        }
+        return smallestNumber;
+    }
+
+    private int getLargestNumber(int size) {
+        int largestNumber = 0;
+        for(int i=9 - size + 1,j=0;j < size ;j++,i++) {
+             largestNumber = (largestNumber * 10) + i;
+        }
+        return largestNumber;
+    }
+
+    private boolean isValid(List<Integer> digits) {
+        for(int i=1;i<digits.size();i++) {
+            if(digits.get(i) >= digits.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public SpecialList(int size) {
+        int currentNumber = getSmallestNumber(size);
+        seq = new ArrayList<>();
+        for(int i=currentNumber;i<=getLargestNumber(size);i++) {
+            if(isValid(toDigits(i))) {
+                seq.add(i);
+            }
+        }
+        System.out.println(seq);
+//        seq.add(currentNumber);
+
+    }
+
+
+
 }
